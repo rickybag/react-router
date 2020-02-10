e7213798056c0d34da343d66c05293fa13e55cac
Riccardo Sacco <rickybag99@gmail.com>
2020-02-10 17:18:50 +0100

    Updates

---
+++ precache-manifest.ec01e9c049e8afb70d88d1054c7fc92a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9acba2e3c93457182eb051aa04c1d7",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "11584210f82ed5ba47a1",
+    "url": "/react-router/static/css/main.96a0b4fb.chunk.css"
+  },
+  {
+    "revision": "e536c7644e4e9fe48448",
+    "url": "/react-router/static/js/2.4e71a9df.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-router/static/js/2.4e71a9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11584210f82ed5ba47a1",
+    "url": "/react-router/static/js/main.3ce05854.chunk.js"
+  },
+  {
+    "revision": "382d1f0d9a077c8e52ef",
+    "url": "/react-router/static/js/runtime-main.51193319.js"
+  }
+]);
